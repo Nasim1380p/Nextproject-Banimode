3267fdc6d1c034d11110d6699a4bd2445b28e6f7
nasim <155636802+Nasim1380p@users.noreply.github.com>
2024-09-26 13:39:38 -0700

    Update Brands.jsx

--- src/app/components/Brands.jsx
+++ src/app/components/Brands.jsx
@@ -6,7 +6,7 @@ export default function Brand() {
   return (
     <section className='w-full flex justify-center flex-wrap mt-10 mb-10 md:mt-16 md:mb-16 bg-[#f9f9f9]'>
        <div className='mt-12 mb-12 w-full'>
-       <h2 className='w-full text-center text-xl font-semibold md:text-2xl'>برترین برندها در بانی‌مد</h2>
+       <h2 className='w-full text-center text-xl font-semibold md:text-2xl '>برترین برندها در بانی‌مد</h2>
        <div className='w-full flex justify-center'>
        <div className='md:w-[80%] w-[90%] flex items-center h-[140px] mt-8'>
            <Sliderbrand/>
